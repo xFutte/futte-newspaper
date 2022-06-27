ee54b9ee725d7fae482f29c30cfa82bd38514deb
Christian <Futte8D@hotmail.com>
2022-06-27 21:24:25 +0200

    Updated manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,13 +9,11 @@ author 'xFutte'
 description 'Open news stand to read release notes'
 version '1.0'
 
-client_script {'@PolyZone/client.lua', '@PolyZone/BoxZone.lua', '@PolyZone/EntityZone.lua', '@PolyZone/CircleZone.lua',
-               '@PolyZone/ComboZone.lua', 'client/client.lua', 'client/html/index.html', 'client/html/styles.css',
-               'client/html/bg.jpg', 'client/html/app.js'}
+shared_script 'config.lua'
 
-server_script {
-       'server/server.lua'
-}
+client_script {'@PolyZone/client.lua', '@PolyZone/EntityZone.lua', 'client/client.lua'}
+
+server_script {'server/server.lua'}
 
 files {'client/client.lua', 'client/html/index.html', 'client/html/styles.css', 'client/html/app.js',
        'client/html/bg.jpg'}
