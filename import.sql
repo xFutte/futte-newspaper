5f9f0171e9f8697edcafaee888a07c93224d994c
Christian <Futte8D@hotmail.com>
2022-07-13 23:56:05 +0200

    Update import.sql

--- import.sql
+++ import.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `newsstands` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`story_type` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
-	`header` VARCHAR(5000) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`title` VARCHAR(5000) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
 	`body` VARCHAR(5000) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
 	`date` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8_general_ci',
 	`jailed_player` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
