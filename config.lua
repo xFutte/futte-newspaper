996656dabcaaa43f08878139103435eb03bab593
Christian <Futte8D@hotmail.com>
2022-07-22 18:02:34 +0200

    Added amount of X to configuration

--- config.lua
+++ config.lua
@@ -3,3 +3,5 @@ Config = {}
 Config.BuyNewspaperText = 'Buy newspaper' -- Text shown with qb-target
 Config.BuyNewspaperIcon = 'fas fa-newspaper' -- Icon shown with qb-target
 Config.Price = 100 -- Price of buying the newspaper
+Config.AmountOfNews = 10
+Config.AmountOfSentences = 10
