ac963abc1f14d91d501533c94f739d4eee26c1ab
Christian <Futte8D@hotmail.com>
2022-07-22 18:06:06 +0200

    Added extra reporter checks

--- web/src/components/Content.svelte
+++ web/src/components/Content.svelte
@@ -13,12 +13,16 @@
 	import { mdiStar } from '@mdi/js';
 	import type { Story } from '../interfaces/story';
 	import { Config } from '../../config';
+	import PrisonSentences from '../pages/PrisonSentences.svelte';
+	import type { ISentence } from '../interfaces/ISentence';
 
 	let value = 0;
 
 	export let stories: Array<Story>;
 	export let isReporter: boolean;
+	export let reporterOnDuty: boolean;
 	export let reporterLevel: number;
+	export let sentences: Array<ISentence>;
 </script>
 
 <AppBar>
@@ -27,13 +31,13 @@
 		<Tabs class="green-text" bind:value fixedTabs>
 			<div slot="tabs">
 				<Tab>{Config.text.tabs.newspaper}</Tab>
-				{#if Config.tabs.showPrisonSentences}<Tab disabled
+				{#if Config.tabs.showPrisonSentences}<Tab
 						>{Config.text.tabs.prisonSentences}</Tab
 					>{/if}
 				{#if Config.tabs.showCityNews}<Tab disabled
 						>{Config.text.tabs.cityUpdates}</Tab
 					>{/if}
-				{#if isReporter == true}<Tab
+				{#if isReporter && reporterOnDuty}<Tab
 						><Icon path={mdiStar} /><span class="ml-2"
 							>{Config.text.tabs.reporterActions}</span
 						></Tab
@@ -48,15 +52,7 @@
 		<News {stories} />
 	</WindowItem>
 	<WindowItem>
-		<h4>Prison sentences</h4>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-			accumsan, diam et elementum gravida, arcu mi fermentum nibh, vel
-			dapibus ligula orci non est. Morbi commodo sagittis finibus.
-			Maecenas in volutpat massa. Nullam vulputate metus velit, quis
-			interdum elit imperdiet ut. Suspendisse et sagittis erat, euismod
-			vulputate enim. Etiam feugiat sit amet justo vitae commodo.
-		</p>
+		<PrisonSentences {sentences} />
 	</WindowItem>
 	<WindowItem>
 		<h4>City updates</h4>
@@ -69,7 +65,9 @@
 			vulputate enim. Etiam feugiat sit amet justo vitae commodo.
 		</p>
 	</WindowItem>
-	<WindowItem>
-		<Reporter {stories} {reporterLevel} />
-	</WindowItem>
+	{#if isReporter}
+		<WindowItem>
+			<Reporter {stories} {reporterLevel} {isReporter} />
+		</WindowItem>
+	{/if}
 </Window>
