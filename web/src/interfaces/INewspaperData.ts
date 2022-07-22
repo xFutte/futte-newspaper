649a911036c62520c7ceaba51db9b4e175c65b96
Christian <Futte8D@hotmail.com>
2022-07-22 18:06:47 +0200

    Added interfaces

---
+++ web/src/interfaces/INewspaperData.ts
@@ -0,0 +1,11 @@
+import type { ISentence } from './ISentence';
+import type { Story } from './story';
+
+export interface INewspaperData {
+	stories: Array<Story>;
+	sentences: Array<ISentence>;
+	jailSentences: Array<any>;
+	reporterLevel: number;
+	reporterOnDuty: boolean;
+	isReporter: boolean;
+}
---
+++ web/src/interfaces/ISentence.ts
@@ -0,0 +1,5 @@
+export interface ISentence {
+	date: string;
+	jailed_player: string;
+	jailed_time: string;
+}
