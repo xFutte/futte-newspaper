01f699f3f437fc6da3b660eeeac08bd64202b0d8
xViperAG <82969741+xViperAG@users.noreply.github.com>
2022-07-27 03:54:04 -0400

    Caps Change for Target

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.BuyNewspaperText = 'Buy newspaper' -- Text shown with qb-target
+Config.BuyNewspaperText = 'Buy Newspaper' -- Text shown with qb-target
 Config.BuyNewspaperIcon = 'fas fa-newspaper' -- Icon shown with qb-target
 Config.Price = 100 -- Price of buying the newspaper
 Config.AmountOfNews = 10 -- Amount of news to be fetched from the database
