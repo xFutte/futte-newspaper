712f1c73b17283514299688cd36ec800ab1ebb5e
NewEditOfficial <newedit@protonmail.com>
2023-11-25 16:59:19 +0100

    NUI
    feat: (NUI) Added resync  of data and handleMenu function which allows to show/hide menu.

--- client/cl-nui.lua
+++ client/cl-nui.lua
@@ -1,12 +1,15 @@
 RegisterNUICallback('publishStory', function(data)
+    ExitUI('refresh')
     TriggerServerEvent('futte-newspaper:server:publishStory', data)
 end)
 
 RegisterNUICallback('updateStory', function(data)
+    ExitUI('refresh')
     TriggerServerEvent('futte-newspaper:server:updateStory', data)
 end)
 
 RegisterNUICallback('deleteStory', function(data)
+    ExitUI('refresh')
     TriggerServerEvent('futte-newspaper:server:deleteStory', data)
 end)
 
@@ -15,7 +18,30 @@ RegisterNUICallback('newspaper:client:closeNewspaper', function(_, cb)
     ExitUI()
 end)
 
-function ExitUI()
+function RefreshStories()
+    local retval = {
+        stories = STORIES_CACHE.NEWS and next(STORIES_CACHE.NEWS) and STORIES_CACHE.NEWS or {},
+        sentences = STORIES_CACHE.SENTENCES and next(STORIES_CACHE.SENTENCES) and STORIES_CACHE.SENTENCES or {},
+    }
+
+    Wait(0)
+
+    SendNUIMessage({
+        action = 'fetchStories',
+        data = retval,
+    })
+end
+
+function HandleMenu(state)
+    SendNUIMessage({
+        action = 'setVisible',
+        data = state,
+    })
+
+    SetNuiFocus(state, state)
+end
+
+function ExitUI(state)
     SetNuiFocus(false, false)
 
     local plyPed = PlayerPedId()
@@ -26,6 +52,14 @@ function ExitUI()
 
     ClearPedTasks(plyPed)
     ClearAnyAttachedEntitiesToPed()
+
+    HandleMenu(false)
+
+    if state == 'refresh' then
+        SetTimeout(1000, function()
+            RefreshStories()
+        end)
+    end
 end
 
 function OpenNewsPaperUI()
@@ -34,12 +68,15 @@ function OpenNewsPaperUI()
     Wait(0)
 
     SendNUIMessage({
-        stories = STORIES_CACHE.NEWS and next(STORIES_CACHE.NEWS) and STORIES_CACHE.NEWS or {},
-        sentences = STORIES_CACHE.SENTENCES and next(STORIES_CACHE.SENTENCES) and STORIES_CACHE.SENTENCES or {},
-        isReporter = job and job.name == Config.Job.name,
-        reporterLevel = job and job.grade.level or 0,
-        reporterOnDuty = job and job.onduty,
-        playerName = playerName or 'UNK-NAME'
+        action = 'open',
+        data = {
+            stories = STORIES_CACHE.NEWS and next(STORIES_CACHE.NEWS) and STORIES_CACHE.NEWS or {},
+            sentences = STORIES_CACHE.SENTENCES and next(STORIES_CACHE.SENTENCES) and STORIES_CACHE.SENTENCES or {},
+            isReporter = job and job.name == Config.Job.name,
+            reporterLevel = job and job.grade.level or 0,
+            reporterOnDuty = job and job.onduty,
+            playerName = playerName or 'UNK-NAME',
+        },
     })
 
     SetNuiFocus(true, true)
